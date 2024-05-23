b44a07a530070836e73796f6b73d7ec915482bb1
Minghe <mmh@MKP.local>
2024-05-23 19:20:16 -0400

    initial change

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+
     public static long sum(List<Integer> numbers) {
 
         int s = 0;
